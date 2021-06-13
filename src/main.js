15718506afe258adf1fc988f276c9dd0467d0102
Humberto Angelucci <angeluccirincon@gmail.com>
2021-06-13 13:52:31 +0200

    add background image

--- src/main.js
+++ src/main.js
@@ -1,4 +1,50 @@
 //Canvas 
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
-canvas.style.border = '1px solid black'; // delete 
+ 
+
+//--------------
+//   Images
+//--------------
+
+let ground = new Image();
+ground.src  = '../img/background/ground.png'
+
+let sun = new Image();
+sun.src  = '../img/background/sun.png'
+
+//--------------
+//  Audio
+//--------------
+
+
+
+//--------------
+//  Variables
+//--------------
+let intervalId = 0;
+let isGameOver = false;
+
+//--------------
+// functions
+//--------------
+
+function draw(){
+
+    ctx.drawImage( sun, -25, -20 )
+    //ctx.drawImage( ground, 0, 0 )
+
+    if (gameOver) {
+        cancelAnimationFrame(intervalId)
+    }else{
+        intervalId = requestAnimationFrame(draw)
+    }   
+}
+
+//--------------
+// Event Listener
+//--------------
+
+window.addEventListener('load', () => {
+    draw()
+})
